9ab308d762d0b0eec897d3caff22f137db117ff4
Simon Coltman <simon.coltman@gmail.com>
2017-05-18 21:38:41 +0100

    put questions into a separate file

---
+++ quizQuestions.js
@@ -0,0 +1,445 @@
+var quizQuestions = {
+  'questions' :[
+  {
+    'question' : [
+      { 'text' : 'Lilly is Patrick’s sister',
+        'type' : 'Premise'
+
+      },
+      {
+        'text' : 'Patrick is Sam’s brother',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, Lilly is Sam’s sister',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Sound'
+  },
+  {
+    'question' : [
+      { 'text' : 'If there is a purple elephant in the hall, then I am a giant turkey',
+        'type' : 'Premise'
+
+      },
+      {
+        'text' : 'There is a purple elephant in the hall',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, I’m a giant turkey',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Valid'
+  },
+  {
+    'question' : [
+      { 'text' : 'I have a very strong feeling that my lottery ticket is the winning ticket',
+        'type' : 'Premise'
+
+      },
+      {
+        'text' : 'So, I’m quite confident I will win a lot of money tonight',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Invalid'
+  },
+  {
+    'question' : [
+      { 'text' : 'I have a friend that speaks French and English',
+        'type' : 'Premise'
+      },
+      { 'text' : 'My friend is a human',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'So, at least one human speaks two languages',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Sound'
+  },
+  {
+    'question' : [
+      { 'text' : 'Bees buzz as they fly around',
+        'type' : 'Premise'
+      },
+      { 'text' : 'I can here a buzzing sound',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, there must be a bee nearby',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Invalid'
+  },
+  {
+    'question' : [
+      { 'text' : 'Bees have sharp stingers',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, they can sting you',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Sound'
+  },
+  {
+    'question' : [
+      { 'text' : 'There is a girl called Sarah with only one eye',
+        'type' : 'Premise'
+      },
+      { 'text' : 'Sarah lives next door to me',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Hence, my neighbour has only one eye',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Valid'
+  },
+  {
+    'question' : [
+      { 'text' : 'The earth is a giant round thing',
+        'type' : 'Premise'
+      },
+      { 'text' : 'All objects sitting on round things will fall off',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, we are all going to fall off the earth',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Valid'
+  },
+  {
+    'question' : [
+      { 'text' : 'John is an only child',
+        'type' : 'Premise'
+      },
+      { 'text' : 'John is not an only child',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, the moon is made of cheese',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Valid'
+  },
+  {
+    'question' : [
+      { 'text' : 'Tom is happy only if he is playing guitar',
+        'type' : 'Premise'
+      },
+      { 'text' : 'Tom is not playing guitar',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'So, Tom is not happy',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Sound'
+  },
+  {
+    'question' : [
+      { 'text' : 'Tom said that he didn’t go to Samantha’s party',
+        'type' : 'Premise'
+      },
+      { 'text' : 'No one at Samantha’s party saw Tom there',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, Tom did not attend Samantha’s party.',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Invalid'
+  },
+  {
+    'question' : [
+      { 'text' : 'My coat is either in the kitchen or the hall',
+        'type' : 'Premise'
+      },
+      { 'text' : 'My coat is not in the kitchen',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'So, My coat is in the hall',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Sound'
+  },
+  {
+    'question' : [
+      { 'text' : 'If all dogs are mammals, then all dogs are reptiles',
+        'type' : 'Premise'
+      },
+      { 'text' : 'All dogs are mammals',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, all dogs are reptiles',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Valid'
+  },
+  {
+    'question' : [
+      { 'text' : 'If all dogs are mammals, then all dogs are animals',
+        'type' : 'Premise'
+      },
+      { 'text' : 'All dogs are animals',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, all dogs are mammals',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Invalid'
+  },
+  {
+    'question' : [
+      { 'text' : 'It’s often good to give to charity',
+        'type' : 'Premise'
+      },
+      { 'text' : 'If it’s often good to give to charity, then the Earth is round',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, the Earth is round',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Sound'
+  },
+  {
+    'question' : [
+      { 'text' : 'Murder is always wrong',
+        'type' : 'Premise'
+      },
+      { 'text' : 'Sometimes murder isn’t wrong',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, the death penalty should be illegal',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Valid'
+  },
+  {
+    'question' : [
+      { 'text' : 'We should try to keep an open mind',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, either rocks exist or rocks don’t exist',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Sound'
+  },
+  {
+    'question' : [
+      { 'text' : 'All cats are mammals',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, some cats are mammals',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Invalid'
+  },
+  {
+    'question' : [
+      { 'text' : 'Sally is Tall',
+        'type' : 'Premise'
+      },
+      { 'text' : 'Sally is a girl',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, Sally is a tall girl',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Sound'
+  },
+  {
+    'question' : [
+      { 'text' : 'All people named Lilly like flowers',
+        'type' : 'Premise'
+      },
+      { 'text' : 'I know a specific person called Lilly',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, she is likes flowers',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Valid'
+  },
+  {
+    'question' : [
+      { 'text' : 'Butter is made from milk',
+        'type' : 'Premise'
+      },
+      { 'text' : 'Honey can be bought at the shop',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, I own a pet snake',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Invalid'
+  },
+  {
+    'question' : [
+      { 'text' : 'All men are mortal',
+        'type' : 'Premise'
+      },
+      { 'text' : 'Socrates is a man',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'So, Socrates is a mortal',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Invalid'
+  },
+  {
+    'question' : [
+      { 'text' : 'All students in this class are girls',
+        'type' : 'Premise'
+      },
+      { 'text' : 'Matilda is in this class',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, Matilda is a girl',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Invalid'
+  },
+  {
+    'question' : [
+      { 'text' : 'The teacher drank coffee this morning',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Hence, Simon drank coffee this morning',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Sound'
+  },
+  {
+    'question' : [
+      { 'text' : 'If there is a hedgehog in my gas tank, then my car will not start',
+        'type' : 'Premise'
+      },
+      { 'text' : 'My car will not start',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Hence, there must be a hedgehog in my gas tank.',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Invalid'
+  },
+  {
+    'question' : [
+      { 'text' : 'Either Athens is in Greece or it is in Turkey',
+        'type' : 'Premise'
+      },
+      { 'text' : 'Athens is in Greece',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, Athens is in Turkey',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Invalid'
+  },
+  {
+    'question' : [
+      { 'text' : 'If the moon is made of green cheese, then cows jump over it',
+        'type' : 'Premise'
+      },
+      { 'text' : 'The moon is made of green cheese',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, cows jump over the moon',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Valid'
+  },
+  {
+    'question' : [
+      { 'text' : 'If your teacher is Simon, then your teacher likes Philosophy',
+        'type' : 'Premise'
+      },
+      { 'text' : 'Your teacher is simon',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, Your teacher likes Philosophy',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Sound'
+  },
+  {
+    'question' : [
+      { 'text' : 'All Dolphin come from Venus',
+        'type' : 'Premise'
+      },
+      { 'text' : 'I am a Dolphin',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'So, I came from Venus',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Valid'
+  },
+  {
+    'question' : [
+      { 'text' : 'If I drink any water I will turn into a fish',
+        'type' : 'Premise'
+      },
+      { 'text' : 'I drink lots of water',
+        'type' : 'Premise'
+      },
+      {
+        'text' : 'Therefore, I will turn into a fish',
+        'type' : 'Conclusion'
+      }
+    ],
+    'answer' : 'Valid'
+  }
+],
+possibleAnswers : ['Invalid', 'Valid', 'Sound']
+};
